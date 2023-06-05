9c904187b52ead356138835f9d2f36483197fed6
Mehmet Cihan Yıldız <129149603+mehmetcihanyildiz@users.noreply.github.com>
2023-06-05 15:28:22 +0300

    Add files via upload

--- Lab2-3.asm
+++ Lab2-3.asm
@@ -3,8 +3,8 @@
 ;registers R0, R1, and R2. Single-step the prog
 ;examine the ports and registers.
 
-MOV P1, #0FFH	 ; make P1 an input port by writing 1 to all bits
-MOV P0, P1		 ; take input from P1 and move to P0, P2, R0, R1, R2
+MOV P1, #0FFH   ; make P1 an input port by writing 1 to all bits
+MOV P0, P1      ; take input from P1 and move to P0, P2, R0, R1, R2
 MOV P2, P1
 MOV R0, P1
 MOV R1, P1
