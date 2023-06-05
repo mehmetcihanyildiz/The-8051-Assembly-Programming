24fd3d53c0a537b2371353e584b31c73df70b414
Mehmet Cihan Yıldız <129149603+mehmetcihanyildiz@users.noreply.github.com>
2023-06-05 15:30:31 +0300

    Add files via upload

--- Lab2-7.asm
+++ Lab2-7.asm
@@ -3,8 +3,8 @@
 ;Whenever it goes high, send a high-to-low pulse to port P1.5 to turn on a buzzer.
 
 
-			SETB P2.3			
-LOOP: 	JNB P2.3, LOOP	;keep monitoring for high
-			SETB P1.5			;set bit P1.5=1
-			CLR P1.5			;make high-to-low
-			SJMP LOOP			;keep repeating
+	SETB P2.3			
+LOOP:	JNB P2.3, LOOP	;keep monitoring for high
+	SETB P1.5	;set bit P1.5=1
+	CLR P1.5	;make high-to-low
+	SJMP LOOP	;keep repeating
