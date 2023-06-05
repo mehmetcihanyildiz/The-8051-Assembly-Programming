8fcf19a2dadd795b2bad4a4a92f72850b9c7e570
Mehmet Cihan Yıldız <129149603+mehmetcihanyildiz@users.noreply.github.com>
2023-06-05 15:28:00 +0300

    Add files via upload

--- Lab2-2.asm
+++ Lab2-2.asm
@@ -4,10 +4,10 @@
 ;Since 700 is larger than 255 (the maximum capacity of any register)
 ;we have to use two registers to create a nested loop structure.
 			
-			MOV A, #55H
-			MOV R0, #35
+	MOV A, #55H
+	MOV R0, #35
 OUTER:	MOV R1, #20
 INNER:	CPL A
-			DJNZ R1, INNER
-			DJNZ R0, OUTER
+	DJNZ R1, INNER
+	DJNZ R0, OUTER
 
