54b2ead56dea4fb83a788367bac04eb1bd6f30f2
Mehmet Cihan Yıldız <129149603+mehmetcihanyildiz@users.noreply.github.com>
2023-06-05 21:30:55 +0300

    Add files via upload

---
+++ Lab4-1.asm
@@ -0,0 +1,30 @@
+;Use P1 and P2 to take input of two numbers and calculate the following operations on these
+;numbers in order.
+; Addition R3
+; Subtraction R4
+; Multiplication R5 (high byte) R6 (low byte)
+; Division R7
+; Modulus R0
+MOV P1, #0FFH
+MOV P2, #0FFH	;make P1 and P2 input ports
+MOV R1, P1
+MOV R2, P2	;Save the inputs from P1 and P2 to R1 and R2 respectively
+MOV A, R1 	; A=R1
+ADD A,R2 		; A=A+R2
+MOV R3, A 	;R3=R1+R2
+MOV A, R1		;A=R1
+CLR C 			;clear the Carry flag
+SUBB A, R2 	;A=R1-R2-C (C:Binary Value of the Carry Flag)
+MOV R4, A 	;R4 = R1-R2-C
+MOV A, R1
+MOV B, R2 	;the multiplied values must be stored in registers A and B
+MUL AB 			;multiplies the contents of A and B. The result is kept in both
+					;registers: B keeps the high bits and A keeps the low bits
+MOV R5, B 	;the high byte is saved in R5
+MOV R6, A 	;the low byte is kept in R6
+MOV A, R1
+MOV B, R2 	;A must have the dividend and B must have the divisor
+DIV AB 			;this command divides A by B, stores the quotient in register
+					;A and the remainder in B
+MOV R7, A 	;division is stored in R7
+MOV R0, B 	;modulus (remainder of A/B) is saved in R0
---
+++ Lab4-3.asm
@@ -0,0 +1,16 @@
+;Assume that R7-R6 and R5-R4 are two 16 bit registers, where R6 and R4 keep lower bytes,
+;R5 and R7 keep the higher bytes. Subtract these two numbers ( R5R4 - R7R6 ) and store the
+;result in RAM locations 33H (lower byte) and 32H (higher byte). Don’t adjust for negative
+;values.
+
+;	   R5 R4
+; 	   R7 R6
+;-__________
+; 	  32H 33H
+
+MOV A, R4 	;A=R4
+SUBB A, R6 	;A=R4-R6-C
+MOV 33H, A 	;store the lower byte in RAM lcation 33H
+MOV A, R5 	;A=R5
+SUBB A, R7	;A=R5-R7-C
+MOV 32H, A 	;store the higher byte
