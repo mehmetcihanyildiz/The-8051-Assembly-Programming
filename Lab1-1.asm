5cb5fde404f8d4f14b60893a2ea8e78766d6150f
Mehmet Cihan Yıldız <129149603+mehmetcihanyildiz@users.noreply.github.com>
2023-06-05 14:49:58 +0300

    Add files via upload

---
+++ Lab1-1.asm
@@ -0,0 +1,9 @@
+;Write and assemble a program to move value 99H to register A. 
+;Then from register A move it to registers R0 – R3. 
+;Use the simulator to single-step the program and examine the registers.
+
+MOV A, #99H
+MOV R0, A
+MOV R1, A
+MOV R2, A
+MOV R3, A
