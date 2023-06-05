b5abf1e048b6f22a1daabff48f289551f858ddc7
Mehmet Cihan Yıldız <129149603+mehmetcihanyildiz@users.noreply.github.com>
2023-06-05 21:31:53 +0300

    Add files via upload

--- Lab3-3.asm
+++ Lab3-3.asm
@@ -1,7 +1,7 @@
 ;Write a program that reflects the status of the switches on P2 to the LEDs connected to P1.
 
 BEGIN:	MOV P1,P2
-			SJMP BEGIN
+	SJMP BEGIN
 
 ;P2 is connected to the switch and P1 is connected to the LEDs. Note that when the switch is
 ;closed, P2 holds the value 0. When 0 is moved to P1, the LEDs are turned on. Hence, a
